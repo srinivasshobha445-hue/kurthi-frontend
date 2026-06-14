885919859e551cb72782ace717c0f4ef017f68ec
srinivasshobha445-hue <srinivasshobha445@gmail.com>
2026-06-14 23:18:51 +0530

    order

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCategories } from "../api/category";
 
-const Categories = ({ limit = 7 }) => {
+const Categories = ({ limit = 10 }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
